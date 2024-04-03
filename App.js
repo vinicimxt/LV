439a71353b645e24beb8c276711a6746c9d64ade
unknown <pedro.gomes137@etec.sp.gov.br>
2024-04-02 22:14:15 -0300

    aula0204

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/Components/Title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Title></Title>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#add8e6',
     alignItems: 'center',
     justifyContent: 'center',
   },
